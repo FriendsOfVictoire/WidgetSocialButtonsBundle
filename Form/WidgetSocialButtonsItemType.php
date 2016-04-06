c6bde2b289b05143ef2249d7b7958985046bb5ec
Charlie <charlielucas.cl@gmail.com>
2016-04-06 15:21:21 +0200

    do not extend widget type

--- Form/WidgetSocialButtonsItemType.php
+++ Form/WidgetSocialButtonsItemType.php
@@ -2,13 +2,13 @@
 
 namespace Victoire\Widget\SocialButtonsBundle\Form;
 
+use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Victoire\Bundle\CoreBundle\Form\EntityProxyFormType;
-use Victoire\Bundle\CoreBundle\Form\WidgetType;
 
-class WidgetSocialButtonsItemType extends WidgetType
+class WidgetSocialButtonsItemType extends AbstractType
 {
     /**
      * define form fields.
@@ -77,12 +77,18 @@ class WidgetSocialButtonsItemType extends WidgetType
      */
     public function configureOptions(OptionsResolver $resolver)
     {
-        parent::configureOptions($resolver);
 
         $resolver->setDefaults([
             'data_class'         => 'Victoire\Widget\SocialButtonsBundle\Entity\WidgetSocialButtonsItem',
             'widget'             => null,
             'translation_domain' => 'victoire',
+        ]);
+
+        $resolver->setDefined([
+            'widget',
+            'mode',
+            'namespace',
+            'businessEntityId',
         ]);
     }
 }
